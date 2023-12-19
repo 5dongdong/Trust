34c34e10191c029cc273d91c72cb1fbf1e15b2c7
오동현 <dhgus44@naver.com>
2023-12-20 01:16:51 +0900

    소스코드 추가

---
+++ src/test/java/com/example/truststock/TrustStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.truststock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustStockApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
