fd0a70cc65e6ecd5105489741ee796f50c2779a3
오동현 <dhgus44@naver.com>
2023-12-21 20:29:32 +0900

    이론재고 코드변경

--- src/main/java/com/example/truststock/domain/model/theoretical/TheoreticalInventory.java
+++ src/main/java/com/example/truststock/domain/model/theoretical/TheoreticalInventory.java
@@ -1,16 +1,22 @@
-package com.example.truststock.domain.model;
+package com.example.truststock.domain.model.theoretical;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class TheoreticalInventory {
 
   @Id
-  private Long theoreticalInventoryID;
+  @GeneratedValue
+  @Column(name = "THEORETICALINVENTORY_ID")
+  private Long id;
 
   private int productId;
 
