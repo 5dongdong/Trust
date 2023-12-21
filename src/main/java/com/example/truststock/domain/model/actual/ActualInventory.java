f16c2e688f09d6353669ec46a0dd30a205ac6440
오동현 <dhgus44@naver.com>
2023-12-21 20:28:58 +0900

    코드 변경

--- src/main/java/com/example/truststock/domain/model/actual/ActualInventory.java
+++ src/main/java/com/example/truststock/domain/model/actual/ActualInventory.java
@@ -1,15 +1,21 @@
-package com.example.truststock.domain.model;
+package com.example.truststock.domain.model.actual;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class ActualInventory {
 
   @Id
-  private Long actualInventoryID;
+  @GeneratedValue
+  @Column(name = "ACTUALInvenTORY_ID")
+  private Long id;
 
   private int productId;
 
