985402d50100efa1b2b7f142d00d12b7b7d04e89
오동현 <dhgus44@naver.com>
2023-12-21 20:29:15 +0900

    코드 변경

--- src/main/java/com/example/truststock/domain/repository/ProductRepository.java
+++ src/main/java/com/example/truststock/domain/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.example.truststock.domain.repository;
 
-import com.example.truststock.domain.model.Product;
+import com.example.truststock.domain.model.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
