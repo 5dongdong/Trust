849b0a03859280f7fa3d8f3924254fa4f89fe3fc
오동현 <dhgus44@naver.com>
2023-12-25 23:28:45 +0900

    feat: 이론service 작성

---
+++ src/main/java/com/example/truststock/domain/model/theoretical/TheoreticalInventoryService.java
@@ -0,0 +1,11 @@
+package com.example.truststock.domain.model.theoretical;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TheoreticalInventoryService {
+
+  //음 이론재고를 계산하는 로직을 넣어두자
+  //메뉴가 들어오면 db에서 메뉴의 id값을 불러오고 다른 테이블에 저장된 재료에서 레시피만큼의 재고를 뺀다.
+
+}
